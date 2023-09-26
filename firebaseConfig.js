920c94533890f7020a60c55b92b7a2a9530d2cfc
Ian B <44586707+ibiggy9@users.noreply.github.com>
2023-09-25 20:01:48 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,7 @@ import analytics from '@react-native-firebase/analytics';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCI-TNlmwgmWB9zqg981FoCo_Yya-XcLoI",
-  authDomain: "mental-health-dev-env.firebaseapp.com",
-  projectId: "mental-health-dev-env",
-  storageBucket: "mental-health-dev-env.appspot.com",
-  messagingSenderId: "340004188318",
-  appId: "1:340004188318:web:295b4b8ac7d7ed89038b8f",
-  measurementId: "G-Q5M4HK9Q76"
+ <Your Config>
 };
 
 
